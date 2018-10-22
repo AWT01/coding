df76dbdc32147b82f2c8cf52546f523867e50401
Kevin Herrera <kevin.herrera@fundacion.jala.org>
2018-10-22 12:36:21 -0400

    second part

--- src/main/java/org/fundacionjala/coding/CesarCrypto.java
+++ src/main/java/org/fundacionjala/coding/CesarCrypto.java
@@ -1,19 +1,66 @@
 package org.fundacionjala.coding;
 
 /**
- * @author KevinHerrera - AWT-[01].
- * @version 0.1
+ * Class to encode a message using a key
  */
 public class CesarCrypto {
 
-    public String Encode(String message, int key) {
+    private static final  int CHAR_Z_UPPER_HASHCODE = 90;
+    private static final  int CHAR_A_UPPER_HASHCODE = 65;
+
+    /**
+     * Enum encrypt tasks.
+     */
+    enum EncryptTask { ENCODE, DECODE }
+
+    /**
+     * codifies the alphabetic values of a message using a key.
+     * @param message message to encode
+     * @param key key to encode
+     * @return message as UPPERCASE type encoded
+     */
+    public String encode(final String message, final int key) {
+        return processEncrypt(EncryptTask.ENCODE, message, key);
+    }
+
+    /**
+     * decode the alphabetic values of a message that was encode using a key.
+     * @param encodeMessage message to decode
+     * @param key key to decode
+     * @return message as lowercase type decoded
+     */
+    public String decode(final int key, final String encodeMessage) {
+        return processEncrypt(EncryptTask.DECODE, encodeMessage, key).toLowerCase();
+    }
+
+    /**
+     * internal process of encrypt functions.
+     * @param task encrypt task
+     * @param message message to process
+     * @param key codification key
+     * @return message encrypted or decrypted determinated by task value
+     */
+    private String processEncrypt(final EncryptTask task, final String message, int key) {
         StringBuilder messageBuilder = new StringBuilder();
-        if (message == null) return messageBuilder.toString();
+        if (message == null) {
+            return messageBuilder.toString();
+        }
         for (char c : message.toCharArray()) {
             if (Character.isAlphabetic(c)) {
-                int aux = Character.hashCode(Character.toUpperCase(c));
-                int newValue = aux == 90 ? 64 + key : aux + key;
-                messageBuilder.append(Character.toChars(newValue));
+                int newCharValue = 0;
+                int charHash = Character.hashCode(Character.toUpperCase(c));
+                if (task == EncryptTask.ENCODE) {
+                    int aux = charHash + Math.abs(key);
+                    int aux2 = aux - CHAR_Z_UPPER_HASHCODE;
+                    newCharValue = aux > CHAR_Z_UPPER_HASHCODE ? (CHAR_A_UPPER_HASHCODE - 1) + aux2 : charHash + key;
+                }
+                if (task == EncryptTask.DECODE) {
+                    int aux = charHash - Math.abs(key);
+                    int aux2 = (CHAR_A_UPPER_HASHCODE - 1) - aux;
+                    newCharValue = aux < CHAR_A_UPPER_HASHCODE
+                            ? CHAR_Z_UPPER_HASHCODE - (aux2) : charHash - Math.abs(key);
+                }
+                messageBuilder.append(Character.toChars(newCharValue));
             } else {
                 messageBuilder.append(c);
             }
--- src/test/java/org/fundacionjala/coding/TestCesarCrypto.java
+++ src/test/java/org/fundacionjala/coding/TestCesarCrypto.java
@@ -4,17 +4,40 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * @author KevinHerrera - AWT-[01].
- * @version 0.1
+ * test CesarCrypto class
  */
 public class TestCesarCrypto {
+    private static final int ENCODE_KEY_3 = 3;
+    private static final int ENCODE_KEY_MINUS_3 = -3;
+    private static final int ENCODE_KEY_5 = 5;
+    private static final int ENCODE_KEY_MINUS_5 = -5;
+
+
+    /**
+     * test encode.
+     */
     @Test
-    public void testEncode1() {
+    public void testEncode() {
         CesarCrypto crypto = new CesarCrypto();
-        assertEquals("KROD",crypto.Encode("HOLA",3));
-        assertEquals("ETWWT",crypto.Encode("Zorro",5));
-        assertEquals("!23 ETWWT $56",crypto.Encode("!23 Zorro $56",5));
-        assertEquals("",crypto.Encode("",5));
-        assertEquals("",crypto.Encode(null,5));
+        assertEquals("KROD", crypto.encode("HOLA", ENCODE_KEY_3));
+        assertEquals("ETWWT", crypto.encode("Zorro", ENCODE_KEY_5));
+        assertEquals("DFYJ", crypto.encode("Yate", ENCODE_KEY_5));
+        assertEquals("!23 ETWWT $56", crypto.encode("!23 Zorro $56", ENCODE_KEY_5));
+        assertEquals("", crypto.encode("", ENCODE_KEY_5));
+        assertEquals("", crypto.encode(null, ENCODE_KEY_5));
+    }
+
+    /**
+     * test decode.
+     */
+    @Test
+    public void testDecode() {
+        CesarCrypto crypto = new CesarCrypto();
+        assertEquals("hola", crypto.decode(ENCODE_KEY_MINUS_3, "KROD"));
+        assertEquals("zorro", crypto.decode(ENCODE_KEY_5, "ETWWT"));
+        assertEquals("yate", crypto.decode(ENCODE_KEY_5, "DFYJ"));
+        assertEquals("!23 zorro $56", crypto.decode(ENCODE_KEY_MINUS_5, "!23 ETWWT $56"));
+        assertEquals("", crypto.decode(ENCODE_KEY_5, ""));
+        assertEquals("", crypto.decode(ENCODE_KEY_MINUS_5, null));
     }
 }
