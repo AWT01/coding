2b496ffcc1bf59e5f67d9711771aef42f6352c78
FUNDACIONJALA\KevinSanchez <kevin.sanchezpeep@gmail.com>
2018-10-23 12:14:06 -0400

    improved

--- src/test/java/org/fundacionjala/coding/kevincristhian/CesarCryptoTest.java
+++ src/test/java/org/fundacionjala/coding/kevincristhian/CesarCryptoTest.java
@@ -21,14 +21,10 @@ public class CesarCryptoTest {
     @Test
     public void decode() {
         CesarCrypto cesarCrypto = new CesarCrypto();
-        Assert.assertEquals("HOLA", cesarCrypto.decode("KROD", NEGATIVE_THREE));
         Assert.assertEquals(FOX, cesarCrypto.decode("ETWWT", NEGATIVE_FIVE));
         Assert.assertEquals("", cesarCrypto.decode("", THREE));
         Assert.assertEquals("KROD", cesarCrypto.decode("HOLA", THREE));
-        Assert.assertEquals("HOL A", cesarCrypto.decode("HOL A", ZERO));
-        Assert.assertEquals(FOX, cesarCrypto.decode(FOX, FIFTY_TWO));
         Assert.assertEquals("", cesarCrypto.decode("", THREE));
-        Assert.assertEquals("", cesarCrypto.decode(null, THREE));
     }
 
     /**
@@ -38,12 +34,8 @@ public class CesarCryptoTest {
     public void encode() {
         CesarCrypto cesarCrypto = new CesarCrypto();
         Assert.assertEquals("HOLA", cesarCrypto.decode("KROD", NEGATIVE_THREE));
-        Assert.assertEquals(FOX, cesarCrypto.decode("ETWWT", NEGATIVE_FIVE));
-        Assert.assertEquals("", cesarCrypto.decode("", THREE));
-        Assert.assertEquals("KROD", cesarCrypto.decode("HOLA", THREE));
         Assert.assertEquals("HOL A", cesarCrypto.decode("HOL A", ZERO));
         Assert.assertEquals(FOX, cesarCrypto.decode(FOX, FIFTY_TWO));
-        Assert.assertEquals("", cesarCrypto.decode("", THREE));
         Assert.assertEquals("", cesarCrypto.decode(null, THREE));
     }
 }
