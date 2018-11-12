dd6b305781ba0bb3b6f9d96ea0f1ca2be3d17183
Kevin Sanchez <kevin.sanchezpeep@gmail.com>
2018-11-12 05:57:35 -0800

    Kevin s/katana srot (#14)
    * kata
    
    * improve
    
    * change to final
    
    * add comment
    
    * change to final
    
    * change imports
    
    * change sentece
    
    * refactor code
    
    * delte magic number
    
    * delete magic numebr
    
    * change number

---
+++ src/main/java/org/fundacionjala/coding/kevincristhian/Srot.java
@@ -0,0 +1,45 @@
+package org.fundacionjala.coding.kevincristhian;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringJoiner;
+
+/**
+ * sort class.
+ */
+public final class Srot {
+    private static final int THREE = 3;
+
+    /**
+     * constructor.
+     */
+    private Srot() {
+    }
+    /**
+     * sort method.
+     * @param word to order.
+     * @return string
+     */
+    private static String sort(final String word) {
+        int ini = 1;
+        int end = word.length() - 1;
+        final String[] arrayWord = word.split("");
+        Arrays.sort(arrayWord, ini, end, Collections.reverseOrder());
+        return String.join("", arrayWord);
+    }
+
+    /**
+     * main method.
+     * @param words phrase.
+     * @return string.
+     */
+    public static String sortTheInnerContent(final String words) {
+        String[] phrase = words.split(" ");
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        for (String word : phrase) {
+            stringJoiner.add(word.length() > THREE ? sort(word) : word);
+        }
+        return stringJoiner.toString();
+    }
+}
+
---
+++ src/test/java/org/fundacionjala/coding/kevincristhian/SrotTest.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.coding.kevincristhian;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * tests.
+ */
+public class SrotTest {
+
+    /**
+     * test of sort.
+     */
+    @Test
+    public void sortTheInnerContent() {
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr",
+                Srot.sortTheInnerContent("sort the inner content in descending order"));
+        assertEquals("wiat for me", Srot.sortTheInnerContent("wait for me"));
+        assertEquals("tihs ktaa is esay", Srot.sortTheInnerContent("this kata is easy"));
+
+    }
+}
