7c97d041fd7f53a5cca63260ec03b34930ccfc0b
Kevin Sanchez <kevin.sanchezpeep@gmail.com>
2018-11-12 06:12:47 -0800

    Kevin s/kata triangle tester (#16)
    * some exercices
    
    * change construction string
    
    * kata CesarCrypto
    
    * improve decode cesar
    
    * add comments tests
    
    * add comments
    
    * improve styles
    
    * changes stringbuilder
    
    * improve observations
    
    * add abstract class
    
    * improved
    
    * pmdmain fixed
    
    * improve coverage
    
    * rise coverage
    
    * fix spaces
    
    * add test for coverage
    
    * deleted classes

---
+++ src/main/java/org/fundacionjala/coding/kevincristhian/TriangleTester.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.coding.kevincristhian;
+
+/**
+ * Is this a triangle?. 7kyu.
+ */
+public final class TriangleTester {
+    /**
+     * constructor.
+     */
+    private TriangleTester() {
+    }
+
+    /**
+     * returns true if is a triangle.
+     * @param a one side.
+     * @param b one side.
+     * @param c one side.
+     * @return boolean.
+     */
+    public static boolean isTriangle(int a, int  b, int c) {
+        return a + b > c && a + c > b && b + c > a;
+    }
+}
