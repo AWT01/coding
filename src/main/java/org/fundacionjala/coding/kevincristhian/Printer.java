7174945084e6a424677ef2fa00f304c8f39bd39d
Kevin Sanchez <kevin.sanchezpeep@gmail.com>
2018-11-18 12:22:52 -0400

    Kevin s/kata printer (#17)
    * some exercices
    
    * change construction string
    
    * kata CesarCrypto
    
    * improve decode cesar
    
    * add comments tests
    
    * add comments
    
    * improve styles
    
    * changes stringbuilder
    
    * improve observations
    
    * add abstract class
    
    * improved
    
    * pmdmain fixed
    
    * improve coverage
    
    * rise coverage
    
    * fix spaces
    
    * add test for coverage
    
    * deleted classes

---
+++ src/main/java/org/fundacionjala/coding/kevincristhian/Printer.java
@@ -0,0 +1,30 @@
+package org.fundacionjala.coding.kevincristhian;
+
+/**
+ * Printer Errors 7kyu.
+ */
+public final class Printer {
+    /**
+     * constructor.
+     */
+    private Printer() {
+    }
+
+    /**
+     * method to print.
+     * @param s string
+     * @return string.
+     */
+    public static String printerError(final String s) {
+        int dim = s.length();
+        int i = 0;
+        int cont = 0;
+        while (i < dim) {
+            if (s.charAt(i) > 'm' || s.charAt(i) < 'a') {
+                cont++;
+            }
+            i++;
+        }
+        return String.format("%d/%d", cont, dim);
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/kevincristhian/PrinterTest.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.coding.kevincristhian;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * tests.
+ */
+public class PrinterTest {
+
+    /**
+     * method to test.
+     */
+    @Test
+    public void printerError() {
+        String s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+        assertEquals("3/56", Printer.printerError(s));
+        s = "Zaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+        assertEquals("4/56", Printer.printerError(s));
+
+    }
+}
